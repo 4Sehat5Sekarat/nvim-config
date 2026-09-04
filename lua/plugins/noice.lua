eb0422711b727ea993d74fd4e60ad60631896bca
5zfxqlqgw9ru <devantriangga3@gmail.com>
2026-09-04 14:50:10 +0700

    Tiny Changes

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,54 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    views = {
+      cmdline_popup = {
+        position = {
+          row = 5,
+          col = "50%",
+        },
+        size = {
+          width = 40,
+          height = "auto",
+        },
+      },
+      cmdline_popupmenu = {
+        relative = "editor",
+        position = {
+          row = 8,
+          col = "50%",
+        },
+        size = {
+          width = 40,
+          height = 5,
+        },
+        border = {
+          style = "rounded",
+          padding = { 0, 1 },
+        },
+        win_options = {
+          winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+        },
+      },
+      popupmenu = {
+        relative = "editor",
+        position = {
+          row = 8,
+          col = "50%",
+        },
+        size = {
+          width = 40,
+          height = 5,
+        },
+        border = {
+          style = "rounded",
+          padding = { 0, 1 },
+        },
+        win_options = {
+          winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+        },
+      },
+    },
+  },
+}
